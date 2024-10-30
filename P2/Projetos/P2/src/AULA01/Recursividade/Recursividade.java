2e543e55036a2b3b936ed1f20b4c384d2f672261
lucassouzaa70801 <lucassouzaa70801@gmail.com>
2024-10-30 16:42:23 -0300

    Delete P2/Projetos/P2 directory

--- P2/Projetos/P2/src/AULA01/Recursividade/Recursividade.java
+++
@@ -1,9 +0,0 @@
-package AULA01.Recursividade;
-
-import java.util.Scanner;
-
-public class Recursividade {
-    public static void main(String[] args) {
-        Scanner leia = new Scanner(System.in);
-
-}
